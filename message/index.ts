249b3ec136d323e2064faeec64bbb3e48040e5a2
山田 <huangxinhong2125@gmail.com>
2022-11-25 19:25:49 +0800

    add message

---
+++ message/index.ts
@@ -0,0 +1,14 @@
+import { ElMessage, MessageOptions } from 'element-plus';
+
+export function message(message: string,option?:MessageOptions) {
+  ElMessage({ message, ...option });
+}
+export function warningMessage(message: string,option?:MessageOptions ) {
+  ElMessage({ message,...option, type:'warning' });
+}
+export function errorMessage(message: string,option?:MessageOptions) {
+  ElMessage({ message,...option, type:'error' });
+}
+export function infoMessage(message: string,option?:MessageOptions) {
+  ElMessage({ message,...option, type:'info' });
+}
