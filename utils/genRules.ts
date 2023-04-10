2d8364380a089a1468c98e79afe970201fe6c7a0
QC2168 <huangxinhong2125@gmail.com>
2023-04-10 21:48:50 +0800

    genRequiredRule add opt

--- utils/genRules.ts
+++ utils/genRules.ts
@@ -1,25 +1,44 @@
 import { type RuleItem } from 'async-validator';
-export const genRequiredRule = (message: string): RuleItem => {
-    return {
-        required: true,
-        message,
-        trigger: ['blur','change'],
-        transform(value: string) {
-            if (!value) return false;
-            return value.trim();
-        }
-    } as RuleItem
+
+interface genRequiredRuleOption {
+  min: number;
+  max: number;
+  required: boolean;
+  type: string;
+  pattern: string | RegExp;
 }
+export const genRequiredRule = (
+  message: string,
+  opt?: Partial<genRequiredRuleOption>,
+): RuleItem => {
+  const {
+    min, max, pattern, type = 'string', required = true,
+  } = opt || {};
+  return {
+    required,
+    message,
+    pattern,
+    type,
+    min,
+    max,
+    trigger: ['blur', 'change'],
+    transform(value: string) {
+      if (value === null || value === undefined) return '';
+      return value.trim();
+    },
+  } as RuleItem;
+};
 
 interface genRulesType {
-    key: string | symbol,
-    message: string,
-    rules: RuleItem[]
+  key: string | symbol;
+  message: string;
+  rules: RuleItem[];
 }
 
-export const genRules = ({ key, message, rules }: genRulesType): Record<string | symbol, RuleItem[]> => ({
-    [key]: [
-        genRequiredRule(message),
-        ...rules
-    ]
-})
+export const genRules = ({
+  key,
+  message,
+  rules,
+}: genRulesType): Record<string | symbol, RuleItem[]> => ({
+  [key]: [genRequiredRule(message), ...rules],
+});
